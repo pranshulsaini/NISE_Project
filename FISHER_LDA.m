0410c156cf6584cc73caea469f785d1b4ce28d29
pranshulsaini <pranshulsaini17@gmail.com>
2017-12-28 15:57:01 +0100

    Initially, the two time points chosen to create feature matrix were same across all channels. However, I observed that different channels have max difference between error and no error trials at different time points. So, I created different time points for each channel. The performance did not increase much but I think this is the right way to do

--- FISHER_LDA.m
+++ FISHER_LDA.m
@@ -6,22 +6,32 @@ addpath('D:\TUM\3rd Sem\Neuroinspired System Engineering\eeglab14_1_1b');
 eeglab;
 
 %% Loading of data and creating epochs
-EEG = pop_loadset('filename','calibration.set','filepath','C:/Users/user/Dropbox/Semester 3/Neuro Inspired Engineering/NISE_BCI_tutorial/data/');
+clc;
+EEG = pop_loadset('filename','calibration.set','filepath','C:/Users/user/Dropbox/Semester3/Neuro Inspired Engineering/Project/data/');
 
-% S5 are errors and S4 are no_errors. One of these are errors. This information is
+% S5 are errors and S4 are no_errors. This information is
 % in the third row of every trial (EEG.event)
 
 eegplot(EEG.data,'eloc_file',EEG.chanlocs);   % plotting eeg data
 
-EEG_epo_noError = pop_epoch(EEG,{'S  4'},[-0.5 1]); % retrieving the desired epoch with reference as S4 when there was no error
+EEG_epo_noError = pop_epoch(EEG,{'S  4'},[0.0 0.5]); % retrieving the desired epoch with reference as S4 when there was no error
 
-EEG_epo_Error = pop_epoch(EEG,{'S  5'},[-0.5 1]); % retrieving the desired epoch with reference as S5 when there was error
+EEG_epo_Error = pop_epoch(EEG,{'S  5'},[0.0 0.5]); % retrieving the desired epoch with reference as S5 when there was error
+
+n_chan = size(EEG_epo_noError.data, 1);
+n_no_error =  size(EEG_epo_noError.data, 3);
+n_error = size(EEG_epo_Error.data, 3);
+total_trials = n_no_error + n_error; 
+
 
 %% Time course Visualisation
-avg_no_error = mean(EEG_epo_noError.data,3);
+clc;
+avg_no_error = mean(EEG_epo_noError.data,3);  % mean across all trials
+%avg_no_error = EEG_epo_noError.data;
 cz_avg_no_error = avg_no_error(20,:); %Cz channel is at the center of the head. index is 20
 
-avg_error = mean(EEG_epo_Error.data,3);
+avg_error = mean(EEG_epo_Error.data,3); % mean across all trials
+%avg_error = EEG_epo_Error.data;
 cz_avg_error = avg_error(20,:);   %Cz channel is at the center of the head. index is 20
 
 figure;
@@ -35,55 +45,71 @@ ylabel('Signal (microvolts') % y-axis label
 
 
 %% Topographic Visualisation
-time_pt_1 = find(abs(EEG_epo_Error.times - 210.9375)<1.0);   % Finding the proximity of the event
-time_pt_2 = find(abs(EEG_epo_Error.times - 285.2)<1.0);
+clc;
+time_pt_1 = zeros(size(avg_error,1),1);
+time_pt_2 = zeros(size(avg_error,1),1);
 
-avg_tp1_no_error = avg_no_error(:,time_pt_1);
-avg_tp2_no_error = avg_no_error(:,time_pt_2);
+avg_tp1_no_error = zeros(size(avg_error,1),1);
+avg_tp1_error = zeros(size(avg_error,1),1);
+avg_tp2_no_error = zeros(size(avg_error,1),1);
+avg_tp2_error = zeros(size(avg_error,1),1);
 
-avg_tp1_error = avg_error(:,time_pt_1);
-avg_tp2_error = avg_error(:,time_pt_2);
+for i = 1:size(avg_error,1)  % finding max diff points for every channel
+    [~,time_pt_1(i)] = max(avg_no_error(i,:) - avg_error(i,:));
+    [~,time_pt_2(i)] = min(avg_no_error(i,:) - avg_error(i,:));
+    avg_tp1_no_error(i) = avg_no_error(i,time_pt_1(i));
+    avg_tp2_no_error(i) = avg_no_error(i,time_pt_2(i));
+    avg_tp1_error(i) = avg_error(i,time_pt_1(i));
+    avg_tp2_error(i) = avg_error(i,time_pt_2(i));
+end
 
-figure;   
+
+figure;   % green color would represent 0. Towards red = positive; Towards blue = negative
 ax1 = subplot(2,2,1);
-title(ax1,'no Error N2');
-topoplot(avg_tp1_no_error,EEG.chanlocs,'maplimits',[-4 4]);
+title(ax1,'no Error time point 1');
+topoplot(avg_tp1_no_error,EEG.chanlocs,'maplimits',[-4 4], 'conv', 'on');
 
 ax2 = subplot(2,2,2);
-title(ax2,'no Error P3');
-topoplot(avg_tp2_no_error,EEG.chanlocs,'maplimits',[-4 4]);
+title(ax2,'no Error time point 2');
+topoplot(avg_tp2_no_error,EEG.chanlocs,'maplimits',[-4 4],  'conv', 'on');
 
 ax3 = subplot(2,2,3);
-title(ax3,'Error N2');
-topoplot(avg_tp1_error,EEG.chanlocs,'maplimits',[-4 4]);
+title(ax3,'Error time point 1');
+topoplot(avg_tp1_error,EEG.chanlocs, 'maplimits',[-4 4], 'conv', 'on');
 
 ax4 = subplot(2,2,4);
-title(ax4,'Error P3');
-topoplot(avg_tp2_error,EEG.chanlocs,'maplimits',[-4 4]);
+title(ax4,'Error time point 2');
+topoplot(avg_tp2_error,EEG.chanlocs,'maplimits',[-4 4], 'conv', 'on');
 
 %% Creation of feature vector
-no_error_tp1 = EEG_epo_noError.data(:,time_pt_1,:);
-no_error_tp1 = squeeze(no_error_tp1)';
-no_error_tp2 = EEG_epo_noError.data(:,time_pt_2,:);
-no_error_tp2 = squeeze(no_error_tp2)';
-no_error = [no_error_tp1 no_error_tp2];
+clc;
+no_error_tp1 = zeros(size(EEG_epo_noError.data, 3), size(EEG_epo_noError.data, 1)); % number of trials x number of channels
+no_error_tp2 = zeros(size(EEG_epo_noError.data, 3), size(EEG_epo_noError.data, 1)); % number of trials x number of channels
+error_tp1 = zeros(size(EEG_epo_Error.data, 3), size(EEG_epo_Error.data, 1)); % number of trials x number of channels
+error_tp2 = zeros(size(EEG_epo_Error.data, 3), size(EEG_epo_Error.data, 1)); % number of trials x number of channels
 
-error_tp1 = EEG_epo_Error.data(:,time_pt_1,:);
-error_tp1 = squeeze(error_tp1)';
-error_tp2 = EEG_epo_Error.data(:,time_pt_2,:);
-error_tp2 = squeeze(error_tp2)';
-error = [error_tp1 error_tp2];
+for i = 1:size(avg_error,1) 
+    no_error_tp1(:,i) = EEG_epo_noError.data(i,time_pt_1(i),:);
+    no_error_tp2(:,i) = EEG_epo_noError.data(i,time_pt_2(i),:);
+    
+    error_tp1(:,i) = EEG_epo_Error.data(i,time_pt_1(i),:);
+    error_tp2(:,i) = EEG_epo_Error.data(i,time_pt_2(i),:);
+   
+end
 
-feature_mat = [no_error; error];
+no_error = [no_error_tp1 no_error_tp2]; % dimension = trials x (2*channels)
+error = [error_tp1 error_tp2]; %  dimension = trials x (2*channels)
+
+feature_mat = [no_error; error];  %  dimension = trials x (4*channels)
 
 %standardization
-mean_feature_mat = mean(feature_mat);
-mean_feature_mat = repmat(mean_feature_mat,300,1);
+mean_feature_mat = mean(feature_mat);  % dimension = 4*channels
+mean_feature_mat = repmat(mean_feature_mat,total_trials,1);  % 300 are the total number of trials
 std_feature_mat = std(feature_mat);
-std_feature_mat = repmat(std_feature_mat, 300,1);
+std_feature_mat = repmat(std_feature_mat, total_trials,1);
 feature_mat = (feature_mat - mean_feature_mat)./std_feature_mat;
 
-labels = [-1*ones(190,1); 1*ones(110,1)];
+labels = [-1*ones(n_no_error,1); 1*ones(n_error,1)];
 
 figure;  % just to see if the data is normally distributed or not. Otherwise we could do log normalisation
 histogram(feature_mat(:,47));  % It appears that the data is approximately normally distribued. So log normal transformation is not needed. Moreover, it can't be used here because we can't feed negative values to log function
@@ -91,7 +117,7 @@ title('Approximatly gaussian data (Log normal transform not required)')
 
 
 %% plotting rank wise
-addpath('C:\Users\user\Dropbox\Semester 3\Neuro Inspired Engineering\NISE_BCI_tutorial\functions');
+addpath('C:\Users\user\Dropbox\Semester3\Neuro Inspired Engineering\Project\functions');
 [v, rank] = fisherrank(feature_mat, labels);
 figure;
 plot(1:54, v);
@@ -109,10 +135,12 @@ ylabel('2nd best feature'); % y-axis label
 
 %% Creating a classifier
 
+n_feat = size(feature_mat,2);
+
 % choosing best lambda and optimum number of features using Kfold crossvalidation
-class_error = zeros(100,30,10);
+class_error = zeros(100,round(n_feat/2),10);
 for i =1:100   % I am multiplying in the for loop for 0.01. So it is 0.01 change per loop   
-    for j = 1:30 % Run upto 30 to avoid overfitting
+    for j = 1:round(n_feat/2) % Run upto 30 to avoid overfitting
         indices = crossvalind('Kfold', labels, 10); % for 10 fold cv
         for k = 1:10
             test = (indices == k); train = ~test;
@@ -138,7 +166,7 @@ for i = 1:n
 end
 
 figure;
-[X,Y] = meshgrid(1:30,lambda_set);
+[X,Y] = meshgrid(1:round(n_feat/2),lambda_set);
 surf(X,Y,avg_class_error);
 xlabel('Number of features'); % x-axis label
 ylabel('Lambda value'); % y-axis label
@@ -150,24 +178,30 @@ performance_error = sum(y' ~= labels)/300;  % This is the misclassification rate
 fprintf('The misclassification rate is: %f % \n', performance_error);
 
 %% Model Cross-session validation with the recall set
-EEG_recall = pop_loadset('filename','recall.set','filepath','C:/Users/user/Dropbox/Semester 3/Neuro Inspired Engineering/NISE_BCI_tutorial/data/');
-EEG_epo_recall = pop_epoch(EEG_recall,{'S  6'},[-0.5 1]); % Two spaces between S and 6. S6 in this file is the time when the response was made by the user
+EEG_recall = pop_loadset('filename','recall.set','filepath','C:/Users/user/Dropbox/Semester3/Neuro Inspired Engineering/Project/data/');
+EEG_epo_recall = pop_epoch(EEG_recall,{'S  6'},[0.0 0.5]); % Two spaces between S and 6. S6 in this file is the time when the response was made by the user
 
-time_pt_1_recall = find(abs(EEG_epo_recall.times - 210.9375)<1.0);
-time_pt_2_recall = find(abs(EEG_epo_recall.times - 285.2)<1.0);
+total_trials_recall = size(EEG_epo_recall.data, 3);
 
-% forming feature matrix
-EEG_recall_tp1 = EEG_epo_recall.data(:,time_pt_1_recall,:);
-EEG_recall_tp1 = squeeze(EEG_recall_tp1)';
-EEG_recall_tp2 = EEG_epo_recall.data(:,time_pt_2_recall,:);
-EEG_recall_tp2 = squeeze(EEG_recall_tp2)';
-feature_mat_recall = [EEG_recall_tp1 EEG_recall_tp2];
+time_pt_1_recall = time_pt_1;  % we have to check at the same points as we found during training for each channel
+time_pt_2_recall = time_pt_2;
+
+% % forming feature matrix
+EEG_recall_tp1 = zeros(size(EEG_epo_recall.data, 3), size(EEG_epo_recall.data, 1)); % number of trials x number of channels
+EEG_recall_tp2 = zeros(size(EEG_epo_recall.data, 3), size(EEG_epo_recall.data, 1)); % number of trials x number of channels
+
+for i = 1:size(avg_error,1) 
+    EEG_recall_tp1(:,i) = EEG_epo_recall.data(i,time_pt_1(i),:);
+    EEG_recall_tp2(:,i) = EEG_epo_recall.data(i,time_pt_2(i),:); 
+end
+
+feature_mat_recall = [EEG_recall_tp1 EEG_recall_tp2];  %  dimension = trials x (2*channels)
 
 %standardisation
 mean_feature_mat_recall = mean(feature_mat_recall);
-mean_feature_mat_recall = repmat(mean_feature_mat_recall,300,1);
+mean_feature_mat_recall = repmat(mean_feature_mat_recall,total_trials_recall,1);
 std_feature_mat_recall = std(feature_mat_recall);
-std_feature_mat_recall = repmat(std_feature_mat_recall, 300,1);
+std_feature_mat_recall = repmat(std_feature_mat_recall, total_trials_recall,1);
 feature_mat_recall = (feature_mat_recall - mean_feature_mat_recall)./std_feature_mat_recall;
 
 %Dimensionality reduction
